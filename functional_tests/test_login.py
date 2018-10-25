168ded529a586e5a92ad96864b58ec990a148f70
Nate Clark <natec425@gmail.com>
2018-10-25 11:57:39 -0500

    Add: failing login functional test

---
+++ functional_tests/test_login.py
@@ -0,0 +1,31 @@
+from functional_tests import SeleniumTestCase
+from django.contrib.auth import get_user_model
+
+
+class LoginTest(SeleniumTestCase):
+    username = 'natec425'
+    password = 'Goodpassword!123'
+
+    def setUpTestData(self):
+        self.create_user(username=self.username, password=self.password)
+
+    def test_existing_user_can_login(self):
+        self.go_to_login_page()
+        self.fill_out_login_form()
+        self.should_be_on_user_homepage()
+
+    def go_to_login_page(self):
+        self.visit('app:login')
+
+    def fill_out_login_form(self):
+        self.fill_out(
+            'login-form',
+            {
+                'username': self.username,
+                'password': self.password,
+            },
+            submit=True,
+        )
+
+    def should_be_on_user_homepage(self):
+        self.assert_current_view('app:user-home')
