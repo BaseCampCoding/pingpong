b2cc834a2054a3405c97927ac911154218b576eb
Nate Clark <natec425@gmail.com>
2018-11-01 12:29:56 -0500

    Add CORS headers

--- pingpong/settings.py
+++ pingpong/settings.py
@@ -22,11 +22,13 @@ INSTALLED_APPS = [
     'app',
     'rest_framework',
     'rest_framework.authtoken',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -92,5 +94,7 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.TokenAuthentication',
     ]
 }
+
+CROSS_ORIGIN_ALLOW_ALL = True
 
 django_heroku.settings(locals())
