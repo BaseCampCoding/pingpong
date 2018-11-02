646dae1fef5114a39f3b71a16df94eeaa67de8ce
Nate Clark <natec425@gmail.com>
2018-11-02 11:42:46 -0500

    Allow Options on API endpoints for CORS

--- api/permissions.py
+++ api/permissions.py
@@ -4,3 +4,9 @@ from rest_framework.permissions import BasePermission
 class IsReferee(BasePermission):
     def has_object_permission(self, request, view, obj):
         return request.user == obj.referee
+
+
+class IsOptions(BasePermission):
+    def has_object_permission(self, request, view):
+        print(request.method)
+        return request.method == 'OPTIONS'
--- api/views.py
+++ api/views.py
@@ -2,10 +2,10 @@ from rest_framework.generics import CreateAPIView, UpdateAPIView, ListAPIView, R
 from django.contrib.auth import get_user_model
 from rest_framework.authtoken.models import Token
 from app.models import Game
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, OR
 
 from .serializers import UserCreationSerializer, GameCreationSerializer, ScoreGameSerializer, UserSerializer
-from .permissions import IsReferee
+from .permissions import IsReferee, IsOptions
 
 User = get_user_model()
 
@@ -18,7 +18,7 @@ class RegisterView(CreateAPIView):
 class GameCreationView(CreateAPIView):
     queryset = Game.objects.all()
     serializer_class = GameCreationSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [lambda: OR(IsOptions(), IsAuthenticated())]
 
     def perform_create(self, serializer):
         serializer.save(referee=self.request.user, points=[])
@@ -28,17 +28,17 @@ class ScoreGameView(UpdateAPIView):
     queryset = Game.objects.all()
     lookup_url_kwarg = 'id'
     serializer_class = ScoreGameSerializer
-    permission_classes = [IsReferee]
+    permission_classes = [lambda: OR(IsOptions(), IsReferee())]
 
 
 class UserListView(ListAPIView):
-    permission_classes = [IsAuthenticated]
+    permission_classes = [lambda: OR(IsOptions(), IsAuthenticated())]
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
 
 class UserDetailView(RetrieveAPIView):
-    permission_classes = [IsAuthenticated]
+    permission_classes = [lambda: OR(IsOptions(), IsAuthenticated())]
     lookup_url_kwarg = 'id'
     queryset = User.objects.all()
     serializer_class = UserSerializer
